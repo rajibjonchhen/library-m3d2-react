03e1425093383f5dd35bf1e2af3e095149dd3e01
rajibjonchhen <rajib.jonchhen@gmail.com>
2021-12-14 14:06:03 +0000

    parts linked to App.jsx

--- strive-library/src/App.jsx
+++ strive-library/src/App.jsx
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import "bootstrap/dist/css/bootstrap"
-import "react-bootstrap/dist/react-bootstrap"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Nav from '../src/components/Nav'
+import Nav from '../src/components/Carousel'
+import Nav from '../src/components/Footer'
+
+import { Carousel, Nav } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <Nav/>
+      <Carousel/>
+    <Footer />
+
     </div>
   );
 }
--- strive-library/src/components/carousel.jsx
+++ strive-library/src/components/carousel.jsx
@@ -1,15 +1,27 @@
-<Carousel>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=First slide&bg=373940"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
+import { Component } from 'react'
+import { Carousel, Container, Row, Col, ListGroup } from 'react-bootstrap'
+import books from '../components/data/fantasy.json'
+const Carousel = () => { 
+  return (
+    <Container>
+    <Carousel>
+      books.map(book => {
+        <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={books.img}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>{books.title}</h3>
+          <p>Category : {books.category}</p>
+          <p>Price : {books.price}</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      
+      })
   
-
 </Carousel>
+  </Container>
+  )
+}
