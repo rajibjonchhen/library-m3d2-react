68d4ffce671cc2c69e5599dd51ec2da09ad12736
rajibjonchhen <rajib.jonchhen@gmail.com>
2021-12-14 13:24:15 +0000

    bootstrap installed  added

---
+++ strive-library/src/carousel.jsx
@@ -0,0 +1,15 @@
+<Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="holder.js/800x400?text=First slide&bg=373940"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  
+
+</Carousel>
